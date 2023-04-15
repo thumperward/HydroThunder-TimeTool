7a6da361f4d684216329ec95b1446fc7232f2da2
Chris Cunningham <thumperward@hotmail.com>
2023-04-15 23:31:39 +0000

    pylint all done

--- httt/__init__.py
+++ httt/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+"""
+Tool for exporting and importing high score times from Hydro Thunder Arcade as CSV files.
+"""
+
 import sys
 import os
 import os.path
@@ -8,34 +12,48 @@ import datetime
 import csv
 import argparse
 
-read_drive = None
-write_drive = None
-filename = None
-data_csv = None
-args = None
-
 
 def btime(time_bytes):
-    return str(datetime.timedelta(seconds=round(struct.unpack('<f', time_bytes)[0], 2)))[2:][:8]
+    """
+    Convert bytes to time.
+    """
+
+    return str(
+        datetime.timedelta(seconds=round(
+            struct.unpack('<f', time_bytes)[0], 2))
+    )[2:][:8]
 
 
 def timeb(time_seconds):
-    return struct.pack('<f', (datetime.datetime.strptime(time_seconds.strip(), "%M:%S.%f") - datetime.datetime(1900, 1, 1)).total_seconds())
+    """
+    Convert time to bytes.
+    """
+
+    return struct.pack(
+        '<f',
+        (
+            datetime.datetime.strptime(time_seconds.strip(
+            ), "%M:%S.%f") - datetime.datetime(1900, 1, 1)
+        ).total_seconds()
+    )
 
 
 def get_file_size(filename):
     """
-    Get the file size by seeking at end
+    Get the file size by seeking at end.
     """
 
-    fd = os.open(filename, os.O_RDONLY)
+    file_descriptor = os.open(filename, os.O_RDONLY)
     try:
-        return os.lseek(fd, 0, os.SEEK_END)
+        return os.lseek(file_descriptor, 0, os.SEEK_END)
     finally:
-        os.close(fd)
+        os.close(file_descriptor)
 
 
-class ht:
+class HydroThunder:
+    """
+    General data.
+    """
     boats = {
         b'\x00': "Banshee",
         b'\x01': "Tidal Blade",
@@ -70,7 +88,10 @@ class ht:
         130: "End"
     }
 
-    class data:
+    class FieldData:
+        """
+        Field data.
+        """
         start_offset = [530432, 333824]  # In bytes from true end of drive
         size = 8192  # Rough size rounded up to nice number
         # Always present
@@ -92,98 +113,130 @@ class Drive:
     Object for wrapping a drive, disk image, or raw data block
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename, args):
         # May be filepath, drive block device, or raw
         self.filename = str(filename)
         self.size = int(get_file_size(self.filename))
-        self.raw = self.size <= ht.data.size
+        self.raw = self.size <= HydroThunder.FieldData.size
 
         self.blocks = [
-            0 if self.raw else self.size - ht.data.start_offset[0],
-            0 if self.raw else self.size - ht.data.start_offset[1],
+            0 if self.raw else self.size -
+            HydroThunder.FieldData.start_offset[0],
+            0 if self.raw else self.size -
+            HydroThunder.FieldData.start_offset[1],
         ]
 
         print(
-            f"Reading drive: {self.filename}\nSize: {self.size}\nRaw: {self.raw}\nBlock Addr: {self.blocks[args.block]}"
+            f"Reading drive: {self.filename}\nSize: {self.size}\n"
+            f"Raw: {self.raw}\nBlock Addr: {self.blocks[args.block]}"
         )
         self.times = None
         self.time_bytes = None
         self.splits = None
         self.split_bytes = None
 
-    def read_times(self):
+    def read_times(self, args):
+        """
+        Read times from file.
+        """
         self.times = []
-        with open(self.filename, "rb") as f:
+        with open(self.filename, "rb") as file_to_read:
             # Seek to first initial in filename
-            f.seek(self.blocks[args.block]+ht.data.times_offset)
+            file_to_read.seek(
+                self.blocks[args.block]+HydroThunder.FieldData.times_offset)
             scores = 0
-            while scores < ht.data.time_count:
-                boat = f.read(1)  # read boat
+            while scores < HydroThunder.FieldData.time_count:
+                boat = file_to_read.read(1)  # read boat
                 # print (boat_LUT[boat])
-                initials = str(f.read(3), "ascii")  # read initials
+                initials = str(file_to_read.read(3), "ascii")  # read initials
                 # print (initials)
                 # read four bytes for float representing time in seconds
                 # Note: Game rounds weirdly and these results may differ
-                timestamp = btime(f.read(4))
+                timestamp = btime(file_to_read.read(4))
 
-                self.times.append({"Track": ht.tracks[scores-(
-                    scores % 10)], "Initials": initials, "Boat": ht.boats[boat], "Timestamp": timestamp})
+                self.times.append({
+                    "Track": HydroThunder.tracks[scores-(scores % 10)],
+                    "Initials": initials,
+                    "Boat": HydroThunder.boats[boat],
+                    "Timestamp": timestamp
+                })
                 scores += 1
 
         # print(str(self.times))
 
-    def load_times(self, csv_file):
+    def load_times(self, csv_file, args):
+        """
+        Load time data from a CSV file.
+        """
+
         self.times = []
-        with open(csv_file, newline='') as csvfile:
+        with open(csv_file, newline='', encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             self.times.extend(iter(reader))
-        self.byte_times()
+        self.byte_times(args)
         # print(str(self.times))
 
-    def byte_times(self):
+    def byte_times(self, args):
+        """
+        get time bytes.
+        """
         self.time_bytes = bytearray()
         if self.times is None:
-            self.read_times()
+            self.read_times(args)
 
         for row in self.times:
-            self.time_bytes += ht.iboats[row["Boat"]]
+            self.time_bytes += HydroThunder.iboats[row["Boat"]]
             self.time_bytes += row["Initials"].ljust(3).encode("ascii")
             self.time_bytes += timeb(row["Timestamp"])
 
         return self.time_bytes
         # print(self.time_bytes.hex(" "))
 
-    def read_splits(self):
-        self.splits = []
-        with open(self.filename, "rb") as f:
-            # Seek to first initial in filename
-            f.seek(self.blocks[args.block]+ht.data.split_offset)
-            split = 0
-            while split < ht.data.split_count:
-                split_1 = btime(f.read(4))
-                split_2 = btime(f.read(4))
-                split_3 = btime(f.read(4))
-                split_4 = btime(f.read(4))
-                split_5 = btime(f.read(4))
+    def read_splits(self, args):
+        """
+        Read split times from a file.
+        """
 
-                self.splits.append({"Track": ht.tracks[split*10], "Split 1": split_1,
-                                   "Split 2": split_2, "Split 3": split_3, "Split 4": split_4, "Split 5": split_5})
+        self.splits = []
+        with open(self.filename, "rb") as file_to_read:
+            # Seek to first initial in filename
+            file_to_read.seek(
+                self.blocks[args.block]+HydroThunder.FieldData.split_offset)
+            split = 0
+            while split < HydroThunder.FieldData.split_count:
+                split_1 = btime(file_to_read.read(4))
+                split_2 = btime(file_to_read.read(4))
+                split_3 = btime(file_to_read.read(4))
+                split_4 = btime(file_to_read.read(4))
+                split_5 = btime(file_to_read.read(4))
+
+                self.splits.append({
+                    "Track": HydroThunder.tracks[split*10], "Split 1": split_1,
+                    "Split 2": split_2, "Split 3": split_3, "Split 4": split_4, "Split 5": split_5
+                })
                 split += 1
 
         # print(str(self.splits))
 
-    def load_splits(self, csv_file):
+    def load_splits(self, csv_file, args):
+        """
+        Load split times from a CSV file.
+        """
+
         self.splits = []
-        with open(csv_file, newline='') as csvfile:
+        with open(csv_file, newline='', encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             self.splits.extend(iter(reader))
-        self.byte_splits()
+        self.byte_splits(args)
         # print(str(self.splits))
 
-    def byte_splits(self):
+    def byte_splits(self, args):
+        """
+        Get byte splits.
+        """
         self.split_bytes = bytearray()
         if self.splits is None:
-            self.read_splits()
+            self.read_splits(args)
 
         for row in self.splits:
             self.split_bytes += timeb(row["Split 1"])
@@ -195,74 +248,84 @@ class Drive:
         return self.split_bytes
         # print(self.split_bytes.hex(" "))
 
-    def write(self, update):
-        with open(read_drive.filename, "rb") as r:
-            with open(self.filename, "r+b") as w:
+    def write(self, read_drive, write_drive, args):
+        """
+        Write to drive.
+        """
+        with open(read_drive.filename, "rb") as file_to_read:
+            with open(self.filename, "r+b") as file_to_write:
                 # Seek to block
-                r.seek(read_drive.blocks[args.block])
-                w.seek(write_drive.blocks[args.block])
-                for section, byte_count in ht.data.section_bytes.items():
+                file_to_read.seek(read_drive.blocks[args.block])
+                file_to_write.seek(write_drive.blocks[args.block])
+                for section, byte_count in HydroThunder.FieldData.section_bytes.items():
                     if section == "splits" and read_drive.split_bytes:
-                        w.write(read_drive.split_bytes)
-                        r.seek(byte_count, 1)
+                        file_to_write.write(read_drive.split_bytes)
+                        file_to_read.seek(byte_count, 1)
                     elif (
                         section == "splits"
                         or section == "times"
                         and read_drive.time_bytes is None
                         or section != "times"
                     ):
-                        w.write(r.read(byte_count))
+                        file_to_write.write(file_to_read.read(byte_count))
                     else:
-                        w.write(read_drive.time_bytes)
-                        r.seek(byte_count, 1)
+                        file_to_write.write(read_drive.time_bytes)
+                        file_to_read.seek(byte_count, 1)
 
 
-def checksum_calc(drive):
-    with open(drive.filename, "r+b") as f:
-        f.seek(drive.blocks[args.block])
-        header = f.read(8)
+def checksum_calc(drive, args):
+    """
+    Calculate checksums on a drive.
+    """
+    with open(drive.filename, "r+b") as file_to_check:
+        file_to_check.seek(drive.blocks[args.block])
+        header = file_to_check.read(8)
 
-        if (header != ht.data.header):
+        if header != HydroThunder.FieldData.header:
             print(
                 f"ERROR: Bad header [{header.hex(' ')}] @ {hex(drive.blocks[args.block])}"
             )
 
-        f.read(4)
-        checksum_stored = f.read(4)
-        f.read(4)
+        file_to_check.read(4)
+        checksum_stored = file_to_check.read(4)
+        file_to_check.read(4)
 
-        checksum = ht.data.checksum_seed
+        checksum = HydroThunder.FieldData.checksum_seed
         parity = 0
-        int_read = int(ht.data.size/4)
+        int_read = int(HydroThunder.FieldData.size/4)
         for _ in range(int_read):
-            next_int_bytes = f.read(4)
+            next_int_bytes = file_to_check.read(4)
             next_int = int.from_bytes(next_int_bytes, "little", signed=False)
-            checksum = (checksum+next_int)
+            checksum = checksum+next_int
             # Simulate overflows for uint32 type
             if checksum > 0xFFFFFFFF:
                 checksum = (checksum % 0xFFFFFFFF) - 1
             # parity = parity+(next_int % 0x1)# Use mask to force overflow
             if next_int % 2 == 0:
-                parity = not (parity)
+                parity = not parity
 
         if checksum % 2 == 0:
-            parity = not (parity)
+            parity = not parity
 
         checksum = checksum + parity + args.lsb_offset  # Use mask to set parity bit
 
-        f.seek(drive.blocks[args.block]+ht.data.checksum_offset)
-        f.write(checksum.to_bytes(4, "little", signed=False))
+        file_to_check.seek(
+            drive.blocks[args.block]+HydroThunder.FieldData.checksum_offset)
+        file_to_check.write(checksum.to_bytes(4, "little", signed=False))
 
         print("Checksum:")
         print(f'Found: {checksum_stored.hex(" ")}')
-        print("Wrote: {}".format(checksum.to_bytes(
-            4, "little", signed=False).hex(" ")))
+        print(
+            f'Wrote: {checksum.to_bytes(4, "little", signed=False).hex(" ")}')
 
         return checksum.to_bytes(4, "little", signed=False).hex(" ")
 
 
 def csv_write(data, header, csv_file):
-    with open(str(csv_file), 'w', newline='') as csvfile:
+    """
+    Write data to a CSV file.
+    """
+    with open(str(csv_file), 'w', newline='', encoding="utf-8") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=header)
         writer.writeheader()
         for row in data:
@@ -270,6 +333,9 @@ def csv_write(data, header, csv_file):
 
 
 def print_input_error():
+    """
+    Print usage error.
+    """
     print("Error, Need at least one of the following to be set:\n")
     print("    -r , --read")
     print("    -w , --write")
@@ -279,6 +345,9 @@ def print_input_error():
 
 
 def print_usage():
+    """
+    Print help.
+    """
     parser = argparse.ArgumentParser(
         prog='Hydro Thunder Time Tool',
         description="""
@@ -317,6 +386,9 @@ def print_usage():
 
 
 def write_raw(args, read_drive):
+    """
+    Write data directly to a raw device.
+    """
     write_filename = args.write_raw
     # if os.path.isfile(write_filename):
     #    # Update existing file
@@ -326,66 +398,70 @@ def write_raw(args, read_drive):
     #            print("nope")
     # else:
     #     Write new file
-    with open(read_drive.filename, "rb") as r:
-        with open(write_filename, "wb") as w:
-            r.seek(read_drive.blocks[args.block])
-            for section, byte_count in ht.data.section_bytes.items():
+    with open(read_drive.filename, "rb") as file_to_read:
+        with open(write_filename, "wb") as file_to_write:
+            file_to_read.seek(read_drive.blocks[args.block])
+            for section, byte_count in HydroThunder.FieldData.section_bytes.items():
                 if section == "splits" and read_drive.split_bytes:
-                    w.write(read_drive.split_bytes)
-                    r.seek(byte_count, 1)
+                    file_to_write.write(read_drive.split_bytes)
+                    file_to_read.seek(byte_count, 1)
                 elif (
                     section == "splits"
                     or section == "times"
                     and read_drive.time_bytes is None
                     or section != "times"
                 ):
-                    w.write(r.read(byte_count))
+                    file_to_write.write(file_to_read.read(byte_count))
                 else:
-                    w.write(read_drive.time_bytes)
-                    r.seek(byte_count, 1)
+                    file_to_write.write(read_drive.time_bytes)
+                    file_to_read.seek(byte_count, 1)
 
 
 def main():
+    """
+    Main function.
+    """
     # Run argument parsing
     args = print_usage()
 
     if args.boats:
-        for boat in ht.iboats:
+        for boat in HydroThunder.iboats:
             print(boat)
         sys.exit(0)
 
     if args.map_names:
-        for _, track in ht.tracks.items():
+        for _, track in HydroThunder.tracks.items():
             print(track)
         sys.exit(0)
 
     if not args.read and not args.write and not args.write_raw:
         print_input_error()
 
-    write_drive = Drive(args.write) if args.write else None
-    read_drive = Drive(args.read) if args.read else Drive(args.write)
+    write_drive = Drive(args.write, args) if args.write else None
+    read_drive = Drive(args.read, args) if args.read else Drive(
+        args.write, args)
 
-    read_drive.read_times()
-    read_drive.read_splits()
+    read_drive.read_times(args)
+    read_drive.read_splits(args)
 
     if args.times:
         if args.write or args.write_raw:
-            read_drive.load_times(args.times)
+            read_drive.load_times(args.times, args)
         else:
             csv_write(read_drive.times, [
                 "Track", "Initials", "Boat", "Timestamp"], args.times)
 
     if args.splits:
         if args.write or args.write_raw:
-            read_drive.load_splits(args.splits)
+            read_drive.load_splits(args.splits, args)
         else:
             csv_write(read_drive.splits, [
                 "Track", "Split 1", "Split 2", "Split 3", "Split 4", "Split 5"], args.splits)
 
     if args.write:
-        write_drive.write(read_drive)
-        checksum_calc(write_drive)
+        write_drive.write(read_drive, write_drive, args)
+        checksum_calc(write_drive, args)
 
     if args.write_raw:
         write_raw(args, read_drive)
-        checksum_calc(Drive(args.write_raw))
+        checksum_calc(Drive(args.write_raw, args), args)
